3917a6e59e8cfe276efbfdcfd09668b011bf6074
Your Name <you@example.com>
2019-02-01 17:10:36 +0900

    finish tutorial4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,14 +5,21 @@ from . import views
 #アプリの名前空間を指定{% url 'polls:detail}みたいにやるのに必要
 app_name='polls'
 
+# urlpatterns = [
+#     #name引数のおかげで.htmlファイルに検索をかけることができる
+#     #views.index→このアプリの中のview.pyのindex関数を指定している
+#     path('', views.index, name='index'),
+#     # ex: /polls/5/
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
 urlpatterns = [
-    #name引数のおかげで.htmlファイルに検索をかけることができる
-    #views.index→このアプリの中のview.pyのindex関数を指定している
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,24 +5,48 @@ from django.urls import reverse
 # from django.template import loader
 #root ディレクトリからみて.でimportできる
 from .models import Choice,Question
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-    # try:
-    #     question=Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404('Question does not exist')
-    # return render(request, 'polls/detail.html',{'question':question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+#     # try:
+#     #     question=Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404('Question does not exist')
+#     # return render(request, 'polls/detail.html',{'question':question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+
+
+#オブジェクトのリストを表示する
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+#あるタイプのオブジェクトの詳細ページを表示する
+#question_idをpkに変更している
+#template_name属性を指定すると指定されたテンプレート名を使うようになる
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+#DetailViewのサブクラスのなので仕組みはうえと同じ
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
