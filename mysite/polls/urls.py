169e31f53b8157663dc5e6f058cb1e4be6c1ab6a
Your Name <you@example.com>
2019-02-01 13:41:21 +0900

    add view

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,4 +5,10 @@ from . import views
 urlpatterns = [
     #views.index→このアプリの中のapp.pyのindex関数を指定している
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
